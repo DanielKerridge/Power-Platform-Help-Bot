c5649c7ba8ff2a18f831113e7b4503e6bf6a121e
Daniel Kerridge <kerridge@gmail.com>
2022-09-21 16:22:58 +1000

    Create Site Settings - Common Site Settings.js

---
+++ PowerAppsPortals/Site Settings - Common Site Settings.js
@@ -0,0 +1,16 @@
+Web API:
+Webapi/<table>/enabled</table>
+Default: False
+Valid values: True, False
+
+Webapi/<table>/fields</table>
+All attributes: *
+Specific attributes: attr1,attr2,attr3
+
+Example:
+Webapi/contact/enabledtrue
+Webapi/contact/fieldsvel_portalnotificationacknowledgedate
+
+Hint:Use singular when setting up the settings
+Use plural in the code URL
+
