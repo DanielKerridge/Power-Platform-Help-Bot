e5baa7493ffbbda7346ddd2afdac3be4149fb28a
Daniel Kerridge <kerridge@gmail.com>
2022-09-21 16:45:06 +1000

    Create Forms - Regex.js

---
+++ PowerAppsPortals/Forms - Regex.js
@@ -0,0 +1,14 @@
+*Regex validation from the form metadata only works on single line of text
+
+Only allow English characters
+^[A-Za-z]+$
+
+10 digit phone number
+^[6-9]\d{9}$
+
+Positive whole number
+^[1-9]+[0-9]*$
+Please enter a positive whole number in the quantity field
+
+Positive decimal number
+^([0-7](\.\d+)?|8(\.0+)?)$
