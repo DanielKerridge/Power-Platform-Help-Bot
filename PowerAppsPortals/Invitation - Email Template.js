0f24fc8d433d87f2a831f169f3b6009917d8641e
Daniel Kerridge <kerridge@gmail.com>
2022-09-21 16:41:45 +1000

    Create Invitation - Email Template.js

---
+++ PowerAppsPortals/Invitation - Email Template.js
@@ -0,0 +1,11 @@
+You have been invited to our portal. To redeem your invitation, please click the link below.
+
+Click here https://danieltestportal.powerappsportals.com/register/?returnurl=%2f&invitation={Encoded Invitation Code(Encode Invitation Code)}
+
+to redeem your invitation !
+
+Regards,
+
+Portal Team
+
+Send invite process
